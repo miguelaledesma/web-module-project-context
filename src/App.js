1d70adfac8de77101fffd6ca48d80caaf42fc663
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-24 14:14:55 -0700

    mvp met

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -19,7 +20,8 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value = {cart}>
+			<Navigation />
 			<ProductContext.Provider value = {{products, addItem}}>
 			{/* Routes */}
 			<Route exact path="/">
@@ -27,8 +29,9 @@ function App() {
 			</Route>
 			</ProductContext.Provider>
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
+			</CartContext.Provider>
 		</div>
 	);
 }
